97332dd16aa29e0ff57b60f18704f822247b617b
Adrian Wysocki <adrianwysocki1993@gmail.com>
2021-02-09 22:07:46 +0100

    finished exercise

---
+++ assignment/src/App.js
@@ -0,0 +1,22 @@
+import { useCallback, useState } from "react";
+import { Category } from "./components/Category";
+
+function App() {
+  const [categories, setCategories] = useState([
+    {
+      text: "Category article 1",
+    },
+  ]);
+  return (
+    <div className={`container`}>
+      <Category
+        categories={categories}
+        objIndex={0}
+        setCategories={setCategories}
+        fullCategories={categories}
+      />
+    </div>
+  );
+}
+
+export default App;
